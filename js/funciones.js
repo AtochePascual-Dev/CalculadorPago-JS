dae9f641f939b71004d67bafe564fa6fed97c8c1
Atoche Pascual <jmatochepascual@gmail.com>
2023-06-07 05:22:05 -1000

    [ADD] mostrarAlerta

--- js/funciones.js
+++ js/funciones.js
@@ -1,6 +1,7 @@
 // * IMPORTACIONES
 import { mesaHtml, horaHtml } from './variables.js';
 
+// * Cuarda un cliente
 export const guardarCliente = () => {
   const mesa = mesaHtml.value;
   const hora = horaHtml.value;
@@ -9,6 +10,28 @@ export const guardarCliente = () => {
   const existenVacio = [mesa, hora].includes('');
 
   if (existenVacio) {
-    console.log('Todos los campos son obligatorios');
+    mostrarAlerta('Todos los campos son obligatorios');
+    return;
+  }
+};
+
+
+
+// * Muestra una alerta en pantalla
+const mostrarAlerta = (mensaje) => {
+  const existeAlerta = document.querySelector('.invalid-feedback');
+
+  if (!existeAlerta) {
+    const alerta = document.createElement('DIV');
+
+    alerta.classList.add('invalid-feedback', 'd-block', 'text-center');
+    alerta.textContent = mensaje;
+
+    document.querySelector('.modal-body form').appendChild(alerta);
+
+    // Eliminamos la alerta
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
   }
 };
